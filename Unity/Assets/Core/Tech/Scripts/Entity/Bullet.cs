6769c820aa41b763d46bb2db6bda95386deb5923
Zyf <zyfarok@gmail.com>
2021-10-02 21:22:52 +0200

    piou piou

---
+++ Unity/Assets/Core/Tech/Scripts/Entity/Bullet.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody Rgbd;
+    private Vector2 dir;
+
+    public void Init(Vector3 player, Vector3 dir)
+    {
+        this.dir = dir;
+        Rgbd = this.GetComponent<Rigidbody>();
+        Vector3 pos = player + dir;
+        pos.y += 1;
+        Rgbd.position = pos;
+        Rgbd.velocity = dir * 10;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+ }
--- Unity/Assets/Core/Tech/Scripts/Entity/MovingEntity.cs
+++ Unity/Assets/Core/Tech/Scripts/Entity/MovingEntity.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 
 public class MovingEntity : AbstractEntity
 {
-
-    public float Acceleration =10;
-    public float MaxSpeed=5;
+    public float SpeedNormal = 10;
+    public float SpeedIce = 12.5f;
+    public float SpeedLiquid = 6f;
+    public float InertiaNormal = 0.5f;
+    public float InertiaIce = 0.95f;
+    public float deadZone = 0.1f;
+    public float deadZoneAim = 0.15f;
     public Rigidbody Rgbd;
+    private bool button = false;
+    public Bullet bullet;
+    private float angle = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -18,31 +25,47 @@ public class MovingEntity : AbstractEntity
 
     void FixedUpdate()
     {
+        float xDirection = Input.GetAxis("Horizontal") * (Rgbd.position.x < 0 ? SpeedNormal : SpeedIce);
+        float zDirection = Input.GetAxis("Vertical") * (Rgbd.position.x < 0 ? SpeedNormal : SpeedIce);
+        Vector2 direction = new Vector2(xDirection, zDirection);
+        bool move = deadZone * deadZone < direction.sqrMagnitude;
+        xDirection = move ? xDirection : 0;
+        zDirection = move ? zDirection : 0;
+        Move(xDirection, zDirection);
 
-        float xDirection = Input.GetAxisRaw("Horizontal") * Acceleration * 10;
-        float zDirection = Input.GetAxisRaw("Vertical") * Acceleration * 10;
-        if (xDirection != 0 || zDirection != 0)
-            Move(xDirection, zDirection);
-        else
-            Rgbd.velocity /= 2;
+        float xAim = Input.GetAxis("HorizontalAim");
+        float zAim = Input.GetAxis("VerticalAim");
+        Vector2 aim = new Vector2(xAim, zAim);
+        if (deadZoneAim * deadZoneAim < aim.sqrMagnitude)
+        {
+            angle = Mathf.Atan2(xAim, zAim);
+            this.transform.rotation = Quaternion.Euler(0, angle * 180 / Mathf.PI, 0);
+            Debug.Log(xAim + ", " + zAim + ", " + angle);
+        }
+
+        if (button)
+        {
+            var b = Instantiate<Bullet>(bullet);
+            b.Init(Rgbd.position, new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle)));
+        }
+        button = false;
     }
 
+    private void Update()
+    {
+        button |= Input.GetButtonDown("Fire2");
+    }
 
     void Move(float xDirection, float zDirection)
     {
-        Rgbd.AddForce(new Vector3(xDirection, 0, zDirection));
+        //Rgbd.AddForce(new Vector3(xDirection, 0, zDirection));
         
-        Vector3 velocity = Rgbd.velocity;
-
-        if (velocity.sqrMagnitude > MaxSpeed*MaxSpeed)
-        {
-            float factor = MaxSpeed / velocity.magnitude;
-            velocity.x = velocity.x * factor;
-            velocity.z = velocity.z * factor;
-        }
+        Vector3 prevVelocity = Rgbd.velocity;
+        Vector3 targetVelocity = new Vector3(xDirection, 0, zDirection);
+        float Inertia = (Rgbd.position.x < 0 ? InertiaNormal : InertiaIce);
+        Rgbd.velocity = (prevVelocity * Inertia) + (targetVelocity * (1 - Inertia));
         
-        Rgbd.velocity = velocity;
+        //Debug.Log("old : " + prevVelocity + ", target : " + targetVelocity + ", new : " + Rgbd.velocity);
     }
-
 }
 
